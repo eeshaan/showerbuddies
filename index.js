59be4a983b88285d3aa87cb941d9749af5338546
eeshaan <me@eeshaan.life>
2019-10-20 03:16:22 -0500

    update server issue

--- index.js
+++ index.js
@@ -7,19 +7,17 @@ var wfs     = require('water-flow-sensor')
 
 // Callback
 function wfsCb(res) {
-  console.info(`${res.pin} | ${res.model} | ${res.flow} L/m | ${res.volume} L | ${res.pulses}`)
+  console.info(`${sensor.isRunning} | ${res.flow} L/m | ${res.volume} L | Flow rate: ${0.2641720524*sensor.flow} gal/min | Volume: ${0.2641720524*sensor.volume} gal`)
 }
 
 // Access to status and data
 console.info(`Sensor is running: ${sensor.isRunning}`)
 console.info(`Flow rate: ${sensor.flow} L/min`)
 console.info(`Volume: ${sensor.volume} L`)
-// console.info(`Flow rate: ${0.2641720524*sensor.flow} gal/min`)
-// console.info(`Volume: ${0.2641720524*sensor.volume} gal`)
+console.info(`Volume: ${0.2641720524*sensor.volume} gal`)
+
 
 app.get('/', (req, res) => {
-	console.log('YOOOO');
-		res.json({flow: sensor.flow, volume: sensor.volume, running: sensor.isRunning});
+	res.json({flow: sensor.flow, volume: sensor.volume, running: sensor.isRunning});
 });
-
 app.listen(port, () => {console.log('Listening!')});
